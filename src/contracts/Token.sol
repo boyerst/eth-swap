3cc9a8f12565b10f7dcb30cb17a99301f936ea7c
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-03 11:08:38 -0500

    Add Transfer function and event

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -6,12 +6,27 @@ contract DappToken {
   string public symbol = "DAPP";
 
   event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
   );
 
   event Approval(
   );
 
   constructor() public {
+
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value);
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function approve() {
 
   }
 
