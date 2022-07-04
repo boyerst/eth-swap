bb80813ba6b15c4c6d1b1a1b3826ca5a873a0714
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-04 13:42:50 -0500

    Add balanceOf mapping for user addresses

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -14,15 +14,22 @@ contract DappToken {
   event Approval(
   );
 
-  constructor() public {
+  mapping(address => uint256) public balanceOf;
 
+  constructor() public {
+    balanceOf[msg.sender] = totalSupply;
   }
 
   function transfer(address _to, uint256 _value) public returns (bool success) {
+    // Ensure the sender has balance to send
     require(balanceOf[msg.sender] >= _value);
+    // Subtract transfer value from sending address
     balanceOf[msg.sender] -= _value;
+    // Add transfer value to receiving address
     balanceOf[_to] += _value;
+    // emit that transfer event has happened
     emit Transfer(msg.sender, _to, _value);
+    // Return true to indicate successful tx
     return true;
   }
 
