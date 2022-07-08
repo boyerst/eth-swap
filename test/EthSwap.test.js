d73d785abd509be93a17cc36a941271c10f906f6
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-08 14:17:37 -0500

    Refactor tests with before all hook

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -14,16 +14,14 @@ contract('EthSwap', (accounts) => {
 
   let token, ethSwap
 
-  // before(async () => {
-  //   token = await Token.new()    
-  //   ethSwap = await EthSwap.new(token.address)
-  //   await token.transfer(ethSwap.address, tokens('1000000000'))
-  // })
+  before(async () => {
+    token = await Token.new()    
+    ethSwap = await EthSwap.new()
+    await token.transfer(ethSwap.address, '1000000000')
+  })
   
   describe('Token deployment', async () => {
     it('contract has a name', async () => {
-      let token = await Token.new()
-      // console.log("ETHHHHHHSWAP", ethSwap)
       const name = await token.name()
       assert.equal(name, 'DappToken')
     })  
@@ -31,19 +29,10 @@ contract('EthSwap', (accounts) => {
 
   describe('EthSwap deployment', async () => {
     it('contract has a name', async () => {
-      let ethSwap = await EthSwap.new()
-      // console.log("ETHHHHHHSWAP", ethSwap)
       const name = await ethSwap.name()
       assert.equal(name, 'EthSwap')
     })  
-    // it('EthSwap contract has balance of tokens', async () => {
-    //   let balance = await token.balanceOf(ethSwap.address)
-    //   assert.equal(balance.toString(), tokens('1000000000'))
-    // })
     it('EthSwap contract has balance of tokens', async () => {
-      let token = await Token.new()
-      let ethSwap = await EthSwap.new()
-      await token.transfer(ethSwap.address, '1000000000')
       let balance = await token.balanceOf(ethSwap.address)
       assert.equal(balance.toString(), '1000000000')
     })
