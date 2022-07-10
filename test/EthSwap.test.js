196e3850f04020f99706b08da75781453c4fd99a
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-10 13:50:01 -0500

    Add toWei() helper function to truffle tests

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -6,9 +6,11 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-// function tokens(n) {
-//   return web3.utils.toWei(n, 'ether');
-// }
+// We are NOT actually converting Ether, but rather our own token
+  // However we can use the same web3JS helper because our token has 18 decimal places
+function tokens(n) {
+  return web3.utils.toWei(n, 'ether');
+}
 
 contract('EthSwap', (accounts) => {
 
@@ -17,7 +19,11 @@ contract('EthSwap', (accounts) => {
   before(async () => {
     token = await Token.new()    
     ethSwap = await EthSwap.new()
-    await token.transfer(ethSwap.address, '1000000000000000000000000')
+    // We use the tokens() helper function above to covert this number to a more human readable format
+      // From human readable format to machine (EVM) readable format
+          // unsigned integer      ===> wei
+          // (1 million) 1,000,000 ===> 1000000000000000000000000
+    await token.transfer(ethSwap.address, tokens('1000000'))
   })
   
   describe('Token deployment', async () => {
@@ -34,7 +40,7 @@ contract('EthSwap', (accounts) => {
     })  
     it('contract has tokens', async () => {
       let balance = await token.balanceOf(ethSwap.address)
-      assert.equal(balance.toString(), '1000000000000000000000000')
+      assert.equal(balance.toString(),  tokens('1000000'))
     })
   })
 
